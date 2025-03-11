e3530e36cfafff64ca082dd0225447ad9a7fc7ca
Brendon <elkins@webscope.io>
2025-03-11 10:07:11 +0100

    added database, added http communication

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -59,10 +59,16 @@ export class DetailsComponent {
     email: new FormControl(''),
   });
 
-  constructor(private route:ActivatedRoute, private housingService:HousingService) {
-    const housingLocationId = Number(this.route.snapshot.params['id']);
-    this.housingLocation =
-      this.housingService.getHousingLocationById(housingLocationId);
+  constructor(
+    private route: ActivatedRoute,
+    private housingService: HousingService
+  ) {
+    const housingLocationId = parseInt(this.route.snapshot.params['id'], 10);
+    this.housingService
+      .getHousingLocationById(housingLocationId)
+      .then((housingLocation) => {
+        this.housingLocation = housingLocation;
+      });
   }
 
   submitApplication() {
